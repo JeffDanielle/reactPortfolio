e00d7e5f2fb515fc22010564cc28bb2bc511728c
JeffDanielle <bayotjeff@gmail.com>
2024-01-26 16:42:16 +0800

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,31 @@
 import { Link } from 'react-router-dom';
-import { Button, Container, Form, NavDropdown, Offcanvas, Nav, Navbar} from 'react-bootstrap';
+import { Nav, Navbar} from 'react-bootstrap';
+// import React, { useState } from 'react';
 
 const NavigationBar = () => {
     return ( 
-      <Navbar expand="lg" className="d-flex justify-content-end">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-warning' />
-
-      {/* Adjusting the Navbar.Collapse component to make it a sidebar */}
-      <Navbar.Collapse id="basic-navbar-nav" className="d-flex flex-column align-items-end fs-2 px-5 mt-1">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link href="#about" className='text-white mx-3 fw-bold'>About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#myworks" className='text-white mx-3 fw-bold'>Work</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#contact" className='text-white mx-3 fw-bold'>Contact</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="dark" variant='dark' expand='lg'>
+        <Navbar.Brand className='ms-3'>Your Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' className='me-2' />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end fs-3 me-3">
+          <Nav>
+           <Nav.Link href="#about" className='mx-3 fw-bold'>About</Nav.Link>
+           <Nav.Link href="#myworks" className='mx-3 fw-bold'>Work</Nav.Link>
+           <Nav.Link href="#contact" className='mx-3 fw-bold'>Contact</Nav.Link>
+         </Nav>
+       </Navbar.Collapse>
       </Navbar>
+    //   <Navbar bg="dark" variant="dark" expand="lg">
+    //   <Navbar.Brand href="#home">Your Logo</Navbar.Brand>
+    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //   <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end fs-3">
+    //     <Nav>
+    //       <Nav.Link href="#about" className='mx-3 fw-bold'>About</Nav.Link>
+    //       <Nav.Link href="#myworks" className='mx-3 fw-bold'>Work</Nav.Link>
+    //       <Nav.Link href="#contact" className='mx-3 fw-bold'>Contact</Nav.Link>
+    //     </Nav>
+    //   </Navbar.Collapse>
+    //  </Navbar>
      );
 }
  
