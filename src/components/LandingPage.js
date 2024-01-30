39908da432b68115ae9f1417c47cceb7695c5308
JeffDanielle <bayotjeff@gmail.com>
2024-01-30 16:45:24 +0800

    edited some transition animation

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,11 +11,11 @@ const homepage = () => {
         <Container className=''>
         <Row className="d-flex justify-content-center align-items-center">
             <Col xs={12} sm={6} md={6} lg={4} className="mb-3 mb-md-5">
-                <Image src={BgImage} alt="background_image" className='w-100' />
+                <motion.div initial={{opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3, duration: 2 }} ><Image src={BgImage} alt="background_image" className='w-100' /></motion.div>
             </Col>
             <Col xs={12} sm={6} md={12} lg={7.5} className='px-1 ms-md-5 text-start flex-sm-wrap col-lg'>
-                <motion.p initial={{opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 4 }} className="text_header fs-1 fw-bold mb-3"><span className="txt_Hello ">Hello</span>, I'm Jeff!</motion.p>
-                <motion.p initial={{opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 4 }} className="bg-warning text-dark fs-1 fw-bold text-left d-inline-block py-2 px-3"> Crafting things for the web.</motion.p>
+                <motion.p initial={{opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3, duration: 2 }} className="text_header fs-1 fw-bold mb-3"><span className="txt_Hello ">Hello</span>, I'm Jeff!</motion.p>
+                <motion.p initial={{opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3, duration: 2 }} className="bg-warning text-dark fs-1 fw-bold text-left d-inline-block py-2 px-3"> Crafting things for the web.</motion.p>
             </Col>
         </Row>
     </Container>
