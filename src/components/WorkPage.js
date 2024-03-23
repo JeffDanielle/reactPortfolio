9887143cd16cce2008f07ca72b752806d7e74e47
JeffDanielle <bayotjeff@gmail.com>
2024-03-23 22:59:56 +0800

    update project

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import ClassroomifyImg from '../images/classroomify.png'
 import UnitsImg from '../images/units.png'
 import blackjackImg from '../images/blackjack.png'
+import todoImg from '../images/todo-list.png'
 import { Link } from 'react-router-dom'
 
 const MyProjects = () => {
@@ -44,12 +45,12 @@ const MyProjects = () => {
             <Container className='mt-5 pt-5'>
                   <Row>
                      <Col xs={12} lg={6} className='mb-3'>
-                        <Link to='https://unitsconversion.netlify.app' target='_blank'><Image src={UnitsImg} className='img-thumbnail' id='projects-img' alt='classroomify-thumbnail'/></Link>
+                        <Link to='https://simple-todo-listtt.netlify.app' target='_blank'><Image src={todoImg} className='img-thumbnail border border-light-subtle' id='projects-img' alt='classroomify-thumbnail'/></Link>
                      </Col>
                      <Col xs={12} lg={5} className='d-flex align-items-center'>
                         <div className='bg-secondary rounded ms-lg-5'>
-                           <h3 className='bg-warning rounded text-black fs-4 px-2 py-2 fw-bold'>Metric Converter</h3>
-                           <p className='fs-5 text-center ms-2'>A simple metric converter which converts a user input to length, volume, mass.</p>
+                           <h3 className='bg-warning rounded text-black fs-4 px-2 py-2 fw-bold'>Simple todo list</h3>
+                           <p className='fs-5 text-center ms-2'>A simple to do list app developed using reactJs with add and delete feature </p>
                         </div>
                      </Col>
                   </Row>
