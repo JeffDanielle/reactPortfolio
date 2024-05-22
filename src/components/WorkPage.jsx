44954c351cfcd192ab8d708ecbe0fb83615970ab
JeffDanielle <bayotjeff@gmail.com>
2024-05-22 13:29:59 +0800

    Tweaked some border UIs

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -3,9 +3,9 @@ import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ClassroomifyImg from '../images/classroomify.png'
-// import blackjackImg from '../images/blackjack.png'
+import todoImg from '../images/typescriptTodo.png'
 import fastPizza from '../images/fast-react-pizza.png'
-import todoImg from '../images/todo-list.png'
+
 import { Link } from 'react-router-dom'
 
 const MyProjects = () => {
@@ -15,8 +15,8 @@ const MyProjects = () => {
          {/* 1st project */}
          <Container className='mt-3 pt-5'>
             <Row>
-               <Col xs={12} lg={6} className='mb-3'>
-                  <Link to='https://classroomify-v1.netlify.app' target='_blank'><Image src={ClassroomifyImg} className='img-thumbnail' id='projects-img' alt='classroomify-thumbnail' /></Link>
+               <Col xs={12} lg={6} className='mb-3 '>
+                  <Link to='https://classroomify-v1.netlify.app' target='_blank'><Image src={ClassroomifyImg} className='border border-4 rounded border-secondary-subtle' id='projects-img' alt='classroomify-thumbnail' /></Link>
 
                </Col>
                <Col xs={12} lg={5} className='d-flex align-items-center'>
@@ -31,7 +31,7 @@ const MyProjects = () => {
          <Container className='mt-5 pt-5'>
             <Row>
                <Col xs={12} lg={6} className='mb-3'>
-                  <Link to='https://fast-react-pizzaaa.netlify.app' target='_blank'><Image src={fastPizza} className='img-thumbnail' id='projects-img' alt='classroomify-thumbnail' /></Link>
+                  <Link to='https://fast-react-pizzaaa.netlify.app' target='_blank'><Image src={fastPizza} className='border border-4 rounded border-secondary-subtle' id='projects-img' alt='classroomify-thumbnail' /></Link>
                </Col>
                <Col xs={12} lg={5} className='d-flex align-items-center'>
                   <div className='bg-secondary rounded ms-lg-5'>
@@ -45,12 +45,12 @@ const MyProjects = () => {
          <Container className='mt-5 pt-5'>
             <Row>
                <Col xs={12} lg={6} className='mb-3'>
-                  <Link to='https://simple-todo-listtt.netlify.app' target='_blank'><Image src={todoImg} className='img-thumbnail border border-light-subtle' id='projects-img' alt='classroomify-thumbnail' /></Link>
+                  <Link to='https://simple-todo-typescript.netlify.app' target='_blank'><Image src={todoImg} className='border border-4 rounded border-secondary-subtle' id='projects-img' alt='classroomify-thumbnail' /></Link>
                </Col>
                <Col xs={12} lg={5} className='d-flex align-items-center'>
                   <div className='bg-secondary rounded ms-lg-5'>
-                     <h3 className='bg-warning rounded text-black fs-4 px-2 py-2 fw-bold'>Simple todo list</h3>
-                     <p className='fs-5 text-center ms-2'>A simple to do list app developed using reactJs with add and delete feature </p>
+                     <h3 className='bg-warning rounded text-black fs-4 px-2 py-2 fw-bold'>Simple TypeScript todo list</h3>
+                     <p className='fs-5 text-center ms-2'>A simple to do list app developed using reactJs typescript with add and delete feature </p>
                   </div>
                </Col>
             </Row>
